8cc2282c8f4417118a64eca44888cbffa98ba097
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-06-11 17:20:17 -0400

    bootstrapified Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,17 @@ import React from 'react';
 import logo from '../assets/hook.png';
 import './Navbar.css';
 
-const Navbar = (props) => {
-  return (
-    <div className="Navbar">
-      <img src={logo} className="logo" />
-      <h1 className="company-name">Fictional Building Co.</h1>
-    </div>
-  )
+const Navbar = () => {
+	return (
+		<div className="navbar navbar-expand-lg border border-bottom">
+			<span className="navbar-text mx-3">
+				<a className="navbar-brand">
+					<img className="" src={logo} width="50px" height="50px" />
+        </a>
+        <span class="navbar-brand mb-0 h1">Fictional Building Co.</span>
+			</span>
+		</div>
+	);
 };
 
 export default Navbar;
